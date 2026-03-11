a8a2537ce382fed85ecd9eb9015b481b0ca830b0
timsu92 <33785401+timsu92@users.noreply.github.com>
2026-03-11 23:11:55 +0800

    feat: one-off script to install all vim plugins

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+
+cd ~
+ln -sTf "$SCRIPT_DIR/.vimrc" ~/.vimrc
+ln -snf "$SCRIPT_DIR/.vim" ~/.vim
+
+# vim-plug plugins
+vim -c 'PlugInstall' -c 'sleep 3' -c 'qa'
+
+# coc extensions
+vim -c 'execute "CocInstall -sync " . join(g:coc_global_extensions, " ")' -c 'qa'
